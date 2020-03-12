364da3a5844f674187b90d22b717eefd9f0589ef
Rajesh-vk <58765073+Rajesh-vk@users.noreply.github.com>
2020-03-12 15:49:31 +0530

    Getevent get commit.

--- src/app/events/event.service.spec.ts
+++ src/app/events/event.service.spec.ts
@@ -3,10 +3,12 @@ import { TestBed } from '@angular/core/testing';
 import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 
 import { EventService } from './event.service';
+import { environment } from 'src/environments/environment';
 
 describe('EventService', () => {
     let httpTestingController: HttpTestingController;
     let service: EventService;
+    let eventsSummaryUrl;
 
     beforeEach(() => {
     TestBed.configureTestingModule({
@@ -16,10 +18,29 @@ describe('EventService', () => {
 
     httpTestingController = TestBed.get(HttpTestingController);
     service = TestBed.get(EventService);
-
+    eventsSummaryUrl = environment.baseApiUrl + environment.eventApiUrl;
   });
 
     it('should Event service be created', () => {
-    expect(service).toBeTruthy();
-  });
+        expect(service).toBeTruthy();
+    });
+
+    it('should call the getEvent method and reurn a expected Event ', () => {
+            service.getEvent('1').subscribe();
+            const url = `${eventsSummaryUrl}/${1}`;
+            const req =   httpTestingController.expectOne(url);
+            req.flush({
+                id: '1',
+                baseLocation: 'sa',
+                beneficiaryName: 'sdada',
+                venueAddress: 'fyuuy',
+                eventName: 'event1',
+                eventDescription: 'asdsadsa',
+                totalNoVolunteers: 56,
+                totalVolunteHours: 777,
+                totalTravelHours: 100,
+                livesImpacted: 1000,
+        });
+            httpTestingController.verify();
+    });
 });
